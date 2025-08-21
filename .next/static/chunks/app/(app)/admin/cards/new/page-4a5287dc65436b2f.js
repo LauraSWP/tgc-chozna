02a529db00820d6203f2cdc0c93cc0df1fe2136f
Laura Solanes <bkgumiho@gmail.com>
2025-08-21 17:02:24 +0200

    Small changes

--- .next/static/chunks/app/(app)/admin/cards/new/page-4a5287dc65436b2f.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[160],{332:function(e,n,s){Promise.resolve().then(s.t.bind(s,2972,23)),Promise.resolve().then(s.bind(s,7668))}},function(e){e.O(0,[972,722,205,668,971,117,744],function(){return e(e.s=332)}),_N_E=e.O()}]);
